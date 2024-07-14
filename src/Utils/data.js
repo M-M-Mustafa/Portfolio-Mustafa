848dd71047106e370afeb870a6bcf2bba7dfab63
Mustafa <Tommylance456@gmail.com>
2024-07-14 14:45:02 +0500

    json

---
+++ src/Utils/data.js
@@ -0,0 +1,32 @@
+import Skills from "../Components/Skills/Skills";
+
+export SKILLS= [
+    {
+        title :"FrontEnd",
+        icon :"./src/assets/images/frontend-icon.png",
+        description :"I have experience in building responsive and interactive websites using HTML, CSS, and JavaScript.",
+        Skills: [
+        {Skill: "HTML5", percenage: "90%"},
+        {Skill: "CSS3", percenage: "95%"},
+        {Skill: "JavaScript", percenage: "85%"},
+        {Skill: "React Js", percenage: "75%"},
+        {Skill: "Bootstrap", percenage: "80%"},
+        {Skill: "SASS", percenage: "70%"},
+        {Skill: "Tailwind CSS", percenage: "60%"},
+     ],
+    },
+    {
+        title :"BackEnd",
+        icon :"./src/assets/images/backend-icon.png",
+        description :"I have experience in building responsive and interactive websites using HTML, CSS, and JavaScript.",
+        Skills: [
+        {Skill: "HTML5", percenage: "90%"},
+        {Skill: "CSS3", percenage: "95%"},
+        {Skill: "JavaScript", percenage: "85%"},
+        {Skill: "React Js", percenage: "75%"},
+        {Skill: "Bootstrap", percenage: "80%"},
+        {Skill: "SASS", percenage: "70%"},
+        {Skill: "Tailwind CSS", percenage: "60%"},
+     ],
+    },
+],
