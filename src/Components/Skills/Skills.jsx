a520e0c9f88d071e66a74b1114f9d73dc3951403
M.M.Mustafa <Tommylance456@gmail.com>
2024-07-14 17:23:36 +0500

    version2

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Skills.css"
+import SkillCard from './SkillCard'
+import { SKILLS } from '../../Utils/data'
+import SkillInfoCard from './SkillInfoCard/SkillInfoCard';
 
 const Skills = () => {
-  return (
+        const[selectedSkill,setSelectedSkill]=useState(SKILLS[0]) ;
+        
+        const handleSelectSkill=(data)=>{
+            setSelectedSkill(data)} ;
+
+        return (
     <section className='skills-container'>
         <h5>Technical Proficiency </h5>
         <div className='skills-content'>
              <div className="skills">
-
+                  {SKILLS.map((item)=>(
+                     <SkillCard
+                     key={item.title}
+                     iconUrl={item.icon}
+                     title={item.title}
+                     isActive={selectedSkill.title===item.title}
+                     onClick={()=>{
+                       handleSelectSkill(item);
+                     }}
+                   
+                     />
+                  ))}  
              </div>
-             <div className="skills-info">
 
+             <div className="skills-info">
+                   <SkillInfoCard
+                   heading={selectedSkill.title}
+                   skills={selectedSkill.skills}
+                   />
              </div>
         </div>
     </section>
